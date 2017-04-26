7d73790537b91f297ab8e0462f0c7c7846f9a42d
Jeffrey <jeffreyca16@gmail.com>
2017-04-26 11:18:15 -0400

    Flatten nested tables and remove blue colours

--- content.js
+++ content.js
@@ -6,6 +6,7 @@ for (var i=0, max=links.length; i<max; i++) {
 
 var first = document.getElementsByTagName("p")[0];
 var second = document.getElementsByTagName("p")[1];
+var third = document.getElementsByTagName("p")[2];
 
 first.outerHTML = "";
 delete first;
@@ -20,4 +21,46 @@ if (Z === "") {
     second.innerHTML = second.innerHTML.replace(/Course Number: /g,'Course Number: N/A');
 }
 
-console.log("Log: " + Z);
+var table = document.getElementsByTagName("table")[0];
+
+if (table.innerHTML.trim() == ""){
+    console.log("true");
+    table.parentNode.removeChild(table);
+}
+else {
+    var trs = table.getElementsByTagName("tr");
+    
+    var courseRow = trs[1];
+    var courseElements = trs[1].getElementsByTagName("td");
+    
+    var name = courseElements[0].innerHTML.trim();
+    var cat = courseElements[1].innerHTML.trim();
+    var unit = courseElements[2].innerHTML.trim();
+    var title = courseElements[3].innerHTML.trim();
+    
+    console.log(title + " - " + name + " " + cat + ", " + unit + " units");
+    
+    var nested = trs[2].getElementsByTagName("td")[1].getElementsByTagName("table")[0];
+    
+    table.innerHTML = nested.innerHTML;
+    
+    var line1 = name + " " + cat;
+    var line2 = title;
+    var line3 = unit + " units";
+    
+    var insert = `
+        <h2>${line1}</h2>
+        <h3>${line2}</h3>
+        <h4>${line3}</h4>
+    `;
+    table.insertAdjacentHTML('beforebegin', insert);
+}
+
+var date = third.innerHTML.split("Information last updated: ").pop();
+date = date.replace(/<(?:.|\n)*?>/gm, '');
+
+console.log("Date: " + date);
+var d = new Date(date);
+
+third.innerHTML = third.innerHTML.replace(/Information last updated:(.*?)<\/b>/g, "Last updated on: <b>" + d.toDateString() + "</b>");
+
