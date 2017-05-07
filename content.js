39fa0d22d85354c0eee2d571c4cdfbe4d0df8b0e
Jeffrey <jeffreyca16@gmail.com>
2017-05-07 09:38:10 -0400

    More refactoring

--- content.js
+++ content.js
@@ -26,11 +26,11 @@ function main() {
         $('a').contents().unwrap();
     }
 
-    // Global variables
-    var outerTable = document.getElementsByTagName("table")[0];
-
     cleanQueryResults();
     removeHyperlinks();
+
+    // Global variable
+    var outerTable = document.getElementsByTagName("table")[0];
 
     // Invalid query, end script here
     if (outerTable.innerHTML.trim() == "") {
@@ -38,62 +38,247 @@ function main() {
     }
 
     // Variables used for valid queries
-    var innerTable = document.getElementsByTagName("table")[1];
     var mainParagraph = document.getElementsByTagName("p")[1];
     var tableRowElements = mainParagraph.getElementsByTagName("tr");
     var idCount = -1;
     var tableCount = document.getElementsByTagName("table").length - 1;
 
-    function collapseOuterTable(k) {
-        innerTable = document.getElementsByTagName("table")[k + 1];
+    function transformTable() {
+        function getlen(row) {
+            var len = 0;
+            var cells = row.cells;
 
-        function getInnerTableIndex(outerTableRows) {
-            for (var i = 0; i < outerTableRows.length; i++) {
-                if (outerTableRows[i].contains(innerTable)) {
-                    return i;
-                }
+            for (var j = 0, jLen = cells.length; j < jLen; j++) {
+                // This is very important. If you just take length you'll get the
+                // the wrong answer when colspan exists
+                len += cells[j].colSpan;
             }
-            return -1;
+
+            return len;
         }
 
-        var outerTableRows = outerTable.getElementsByTagName("tr");
-        var outerCourseElements = outerTableRows[1].getElementsByTagName("td");
+        function remove(tableIndex, header) {
+            // Get target th with the name you want to remove.
+            var target = $('table').find('th:contains("' + header + '")');
+            // Find its index among other ths 
+            var index = (target).index() + 1;
+            // For each tr, remove all th and td that match the index.
+            $('table th:nth-child(' + index + ')').eq(tableIndex).remove();
+        }
 
-        var name = outerCourseElements[0].innerHTML.trim();
-        var cat = outerCourseElements[1].innerHTML.trim();
-        var unit = outerCourseElements[2].innerHTML.trim();
-        var title = outerCourseElements[3].innerHTML.trim();
+        function collapseOuterTable(table) {
+            var innerTable = document.getElementsByTagName("table")[table + 1];
 
-        var innerTableIndex = getInnerTableIndex(outerTableRows);
-        console.log("Inner Table Index: " + innerTableIndex);
+            function getInnerTableIndex(outerTableRows) {
+                for (var i = 0; i < outerTableRows.length; i++) {
+                    if (outerTableRows[i].contains(innerTable)) {
+                        return i;
+                    }
+                }
+                return -1;
+            }
 
-        var line1 = name + " " + cat;
-        var line2 = title;
-        var line3 = unit + " units";
+            var outerTableRows = outerTable.getElementsByTagName("tr");
+            var outerCourseElements = outerTableRows[1].getElementsByTagName("td");
 
-        var insert = `
-                    <hr>
+            var name = outerCourseElements[0].innerHTML.trim();
+            var cat = outerCourseElements[1].innerHTML.trim();
+            var unit = outerCourseElements[2].innerHTML.trim();
+            var title = outerCourseElements[3].innerHTML.trim();
+
+            var innerTableIndex = getInnerTableIndex(outerTableRows);
+            console.log("Inner Table Index: " + innerTableIndex);
+
+            var line1 = name + " " + cat;
+            var line2 = title;
+            var line3 = unit + " units";
+
+            var insert = '';
+
+            if (table != 0) {
+                insert = `<br>`;
+            }
+
+            insert += `
                     <h2>${line1}</h2>
                     <h3>${line2}</h3>
                     <h4>${line3}</h4>
                     `;
 
-        for (var i = 2; i < innerTableIndex; i++) {
-            var content = outerTableRows[i].innerText;
-            insert += "<h4><b>" + content + "</b></h4>";
+            for (var i = 2; i < innerTableIndex; i++) {
+                var content = outerTableRows[i].innerText;
+                insert += "<h4><b>" + content + "</b></h4>";
+            }
+
+            outerTable.deleteRow(0);
+            outerTable.deleteRow(0);
+            outerTable.deleteRow(0);
+            outerTable.deleteRow(0);
+
+            outerTable.insertAdjacentHTML('beforebegin', insert);
+            outerTable.insertAdjacentHTML('beforebegin', innerTable.outerHTML);
         }
 
-        outerTable.deleteRow(0);
-        outerTable.deleteRow(0);
-        outerTable.deleteRow(0);
-        outerTable.deleteRow(0);
+        function resizeTable(tableRowElements) {
+            for (var i = 1; i < tableRowElements.length; i++) {
+                var colLen = getlen(tableRowElements[i]);
 
-        outerTable.insertAdjacentHTML('beforebegin', insert);
-        outerTable.insertAdjacentHTML('beforebegin', innerTable.outerHTML);
-    }
+                if (colLen < 13) {
+                    var newCol = tableRowElements[i].insertCell();
+                    newCol.colSpan = 13 - colLen;
 
-    for (var b = 0; b < tableCount; b++) {
-        collapseOuterTable(b);
+                    if (tableRowElements[i].getElementsByTagName("td")[0].colSpan == 1) {
+                        idCount++;
+                    }
+                }
+                // Organize columns
+                else {
+                    tableRowElements[i].style.cursor = "pointer";
+                    idCount++;
+                }
+
+                tableRowElements[i].className = idCount;
+            }
+        }
+
+        function mergeEnrolCells(tableRowElements, table) {
+            function getEnrolCapCol(tableRow) {
+                const FIXED_ENROL_CAP_COL = 6;
+
+                var colLen = getlen(tableRow);
+                return FIXED_ENROL_CAP_COL - (colLen - tableRow.cells.length);
+            }
+
+            for (var i = 1; i < tableRowElements.length; i++) {
+                var enrolCapCol = getEnrolCapCol(tableRowElements[i]);
+                var enrolCap;
+                var enrol;
+
+                // No such enrol cap column
+                if (enrolCapCol < 0) {
+                    console.log(enrolCapCol);
+                    tableRowElements[i].deleteCell(tableRowElements[i].cells.length - 1);
+                    continue;
+                }
+
+                enrolCap = tableRowElements[i].getElementsByTagName("td")[enrolCapCol].innerHTML.trim();
+                enrol = tableRowElements[i].getElementsByTagName("td")[enrolCapCol + 1].innerHTML.trim();
+
+                if (!isNaN(enrolCap) && !isNaN(enrol)) {
+                    tableRowElements[i].getElementsByTagName("td")[enrolCapCol + 1].innerHTML = enrol + "/" + enrolCap;
+                }
+                tableRowElements[i].deleteCell(enrolCapCol);
+
+                console.log("Enrolled: " + enrol + "/" + enrolCap);
+            }
+            remove(table, 'Enrl Cap');
+        }
+
+        function mergeWaitlistCells(tableRowElements, table) {
+            function getWaitCapCol(tableRow) {
+                const FIXED_WAIT_CAP_COL = 7;
+
+                var colLen = getlen(tableRow);
+                return FIXED_WAIT_CAP_COL - (colLen - tableRow.cells.length);
+            }
+
+            for (var i = 1; i < tableRowElements.length; i++) {
+                var waitCapCol = getWaitCapCol(tableRowElements[i]);
+                var waitCap;
+                var wait;
+
+                if (waitCapCol < 0) {
+                    tableRowElements[i].deleteCell(tableRowElements[i].cells.length - 1);
+                    continue;
+                }
+
+                waitCap = tableRowElements[i].getElementsByTagName("td")[waitCapCol].innerHTML.trim();
+                wait = tableRowElements[i].getElementsByTagName("td")[waitCapCol + 1].innerHTML.trim();
+
+                if (!isNaN(waitCap) && !isNaN(wait)) {
+                    tableRowElements[i].getElementsByTagName("td")[waitCapCol + 1].innerHTML = wait + "/" + waitCap;
+                }
+                tableRowElements[i].deleteCell(waitCapCol);
+            }
+            remove(table, 'Wait Cap');
+        }
+
+        function splitDateTimeCells(tableRowElements) {
+            const DATE_TIME_COL = 8;
+
+            for (var i = 1; i < tableRowElements.length; i++) {
+                var dateTimeCell = tableRowElements[i].cells[DATE_TIME_COL];
+
+                if (dateTimeCell && dateTimeCell.innerText != "TBA") {
+                    var dateTime = dateTimeCell.innerText;
+                    // Split date/time into time, day of week, and specific dates (for tests)
+                    var exp = /(.*?)([A-Z|a-z][\S]*)[\s]*(.*)/g;
+                    var match = exp.exec(dateTime);
+
+                    var time = match[1];
+                    var days = match[2];
+                    var date = match[3];
+
+                    // Date is given (i.e. for a TST)
+                    if (date) {
+                        var arr = date.split("-");
+                        var beginDate = Date.parseExact(arr[0], 'MM/dd');
+                        var endDate = Date.parseExact(arr[1], 'MM/dd');
+
+                        if (beginDate == endDate) {
+                            date = "(" + beginDate.toString("MMMM d") + ")";
+                        }
+                        else {
+                            date = "(" + beginDate.toString("MMMM d") + " - " + endDate.toString("MMMM d") + ")";
+                        }
+                    }
+
+                    // Update cell content
+                    dateTimeCell.innerHTML = "<span class='under'>" + time + "</span><br>" + days + "<br>" + date;
+
+                    // Original page does not provide AM/PM info, maybe it could be deduced somehow?
+                    // Ambiguity lies in 8-10 AM/PM
+                    $(".under").hover(function () {
+                        $(this).attr('title', 'Check UWFlow or Quest to determine whether classes are AM or PM.');
+                    });
+                }
+            }
+        }
+
+        function invertInstructorNames(tableRowElements) {
+            function invert(name) {
+                var split = name.split(",");
+                var given = split[1];
+                var surname = split[0];
+
+                return given + " " + surname;
+            }
+
+            for (var i = 1; i < tableRowElements.length; i++) {
+                var cell = tableRowElements[i].cells[10];
+
+                if (cell && cell.innerHTML) {
+                    var name = cell.innerHTML;
+                    cell.innerHTML = invert(name);
+                }
+            }
+        }
+
+        // Collapse all tables first
+        for (var t = 0; t < tableCount; t++) {
+            collapseOuterTable(t);
+        }
+
+        // Transform tables for each course
+        for (var t = 0; t < tableCount; t++) {
+            var tableRowElements = document.getElementsByTagName("table")[t].getElementsByTagName("tr");
+
+            resizeTable(tableRowElements);
+            mergeEnrolCells(tableRowElements, t);
+            mergeWaitlistCells(tableRowElements, t);
+            splitDateTimeCells(tableRowElements);
+            invertInstructorNames(tableRowElements);
+        }
     }
 
     function updateText() {
@@ -136,188 +321,6 @@ function main() {
         trimTableWhiteSpaces();
     }
 
-    function transformTable() {
-        function getlen(row) {
-            var len = 0;
-            var cells = row.cells;
-
-            for (var j = 0, jLen = cells.length; j < jLen; j++) {
-                // This is very important. If you just take length you'll get the
-                // the wrong answer when colspan exists
-                len += cells[j].colSpan;
-            }
-
-            return len;
-        }
-
-        function remove(tableIndex, header) {
-            // Get target th with the name you want to remove.
-            var target = $('table').find('th:contains("' + header + '")');
-            // Find its index among other ths 
-            var index = (target).index() + 1;
-            // For each tr, remove all th and td that match the index.
-            $('table th:nth-child(' + index + ')').eq(tableIndex).remove();
-        }
-
-        function resizeTable() {
-            for (var i = 1; i < tableRowElements.length; i++) {
-                var colLen = getlen(tableRowElements[i]);
-
-                if (colLen < 13) {
-                    var newCol = tableRowElements[i].insertCell();
-                    newCol.colSpan = 13 - colLen;
-
-                    if (tableRowElements[i].getElementsByTagName("td")[0].colSpan == 1) {
-                        idCount++;
-                    }
-                }
-                // Organize columns
-                else {
-                    tableRowElements[i].style.cursor = "pointer";
-                    idCount++;
-                }
-
-                tableRowElements[i].className = idCount;
-            }
-        }
-
-        function mergeEnrolCells(p) {
-            var tableRowElements = document.getElementsByTagName("table")[p].getElementsByTagName("tr");
-
-            function getEnrolCapCol(tableRow) {
-                const FIXED_ENROL_CAP_COL = 6;
-
-                var colLen = getlen(tableRow);
-                return FIXED_ENROL_CAP_COL - (colLen - tableRow.cells.length);
-            }
-
-            for (var i = 1; i < tableRowElements.length; i++) {
-                var enrolCapCol = getEnrolCapCol(tableRowElements[i]);
-                var enrolCap;
-                var enrol;
-
-                if (enrolCapCol <= 0) {
-                    continue;
-                }
-
-                enrolCap = tableRowElements[i].getElementsByTagName("td")[enrolCapCol].innerHTML.trim();
-                enrol = tableRowElements[i].getElementsByTagName("td")[enrolCapCol + 1].innerHTML.trim();
-
-                if (!isNaN(enrolCap) && !isNaN(enrol)) {
-                    tableRowElements[i].getElementsByTagName("td")[enrolCapCol + 1].innerHTML = enrol + "/" + enrolCap;
-                }
-                tableRowElements[i].deleteCell(enrolCapCol);
-
-                console.log("Enrolled: " + enrol + "/" + enrolCap);
-            }
-            remove(p, 'Enrl Cap');
-        }
-
-        function mergeWaitlistCells(p) {
-            var tableRowElements = document.getElementsByTagName("table")[p].getElementsByTagName("tr");
-
-            function getWaitCapCol(tableRow) {
-                const FIXED_WAIT_CAP_COL = 7;
-
-                var colLen = getlen(tableRow);
-                return FIXED_WAIT_CAP_COL - (colLen - tableRow.cells.length);
-            }
-
-            for (var i = 1; i < tableRowElements.length; i++) {
-                var waitCapCol = getWaitCapCol(tableRowElements[i]);
-                var waitCap;
-                var wait;
-
-                if (waitCapCol <= 0) {
-                    continue;
-                }
-
-                waitCap = tableRowElements[i].getElementsByTagName("td")[waitCapCol].innerHTML.trim();
-                wait = tableRowElements[i].getElementsByTagName("td")[waitCapCol + 1].innerHTML.trim();
-
-                if (!isNaN(waitCap) && !isNaN(wait)) {
-                    tableRowElements[i].getElementsByTagName("td")[waitCapCol + 1].innerHTML = wait + "/" + waitCap;
-                }
-                tableRowElements[i].deleteCell(waitCapCol);
-            }
-            remove(p, 'Wait Cap');
-        }
-
-        function splitDateTimeCells(p) {
-            var tableRowElements = document.getElementsByTagName("table")[p].getElementsByTagName("tr");
-            const DATE_TIME_COL = 8;
-
-            for (var i = 1; i < tableRowElements.length; i++) {
-                var dateTimeCell = tableRowElements[i].cells[DATE_TIME_COL];
-
-                if (dateTimeCell && dateTimeCell.innerText != "TBA") {
-                    var dateTime = dateTimeCell.innerText;
-                    // Split date/time into time, day of week, and specific dates (for tests)
-                    var exp = /(.*?)([A-Z|a-z][\S]*)[\s]*(.*)/g;
-                    var match = exp.exec(dateTime);
-
-                    var time = match[1];
-                    var days = match[2];
-                    var date = match[3];
-
-                    // Date is given (i.e. for a TST)
-                    if (date) {
-                        var arr = date.split("-");
-                        var beginDate = Date.parseExact(arr[0], 'MM/dd');
-                        var endDate = Date.parseExact(arr[1], 'MM/dd');
-
-                        if (beginDate == endDate) {
-                            date = "(" + beginDate.toString("MMMM d") + ")";
-                        }
-                        else {
-                            date = "(" + beginDate.toString("MMMM d") + " - " + endDate.toString("MMMM d") + ")";
-                        }
-                    }
-
-                    // Update cell content
-                    dateTimeCell.innerHTML = "<span class='under'>" + time + "</span><br>" + days + "<br>" + date;
-
-                    // Original page does not provide AM/PM info, maybe it could be deduced somehow?
-                    // Ambiguity lies in 8-10 AM/PM
-                    $(".under").hover(function () {
-                        $(this).attr('title', 'Check UWFlow or Quest to determine whether classes are AM or PM.');
-                    });
-                }
-            }
-        }
-
-        function invertInstructorNames(p) {
-            var tableRowElements = document.getElementsByTagName("table")[p].getElementsByTagName("tr");
-
-            function invert(name) {
-                var split = name.split(",");
-                var given = split[1];
-                var surname = split[0];
-
-                return given + " " + surname;
-            }
-
-            for (var i = 1; i < tableRowElements.length; i++) {
-                var cell = tableRowElements[i].cells[10];
-
-                if (cell && cell.innerHTML) {
-                    var name = cell.innerHTML;
-                    cell.innerHTML = invert(name);
-                }
-            }
-        }
-
-        resizeTable();
-
-
-        for (var p = 0; p < tableCount; p++) {
-            mergeEnrolCells(p);
-            mergeWaitlistCells(p);
-            splitDateTimeCells(p);
-            invertInstructorNames(p);
-        }
-    }
-
     function setToggleBehaviour() {
         function setCursors() {
             // Do not show pointer cursor if no child elements to hide
@@ -343,8 +346,14 @@ function main() {
         setToggleListeners();
     }
 
-    updateText();
+    /*
+        for (var b = 0; b < tableCount; b++) {
+            collapseOuterTable(b);
+        }
+    */
+
     transformTable();
+    updateText();
     setToggleBehaviour();
 }
 
